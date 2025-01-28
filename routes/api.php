03c2c436939e4248b74b527ea3483e9689a94ae1
unknown <benyoussefmeriem27@gmail.com>
2025-01-28 16:40:51 +0100

    always the token

--- routes/api.php
+++ routes/api.php
@@ -23,15 +23,15 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 // Routes accessibles à tous les utilisateurs authentifiés
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/tasks', [TaskController::class, 'index'])->name('tasks');;
-    Route::get('/task/{id}', [TaskController::class, 'show']);
+    Route::get('/tasks/{id}', [TaskController::class, 'show']);
 });
 
 
 // Routes accessibles uniquement aux utilisateurs authentifiés (avec le rôle spécifié)
 Route::middleware(['auth:sanctum', 'role:ADMIN'])->group(function () {
     Route::post('/tasks', [TaskController::class, 'store']); // Créer une nouvelle tâche (admin)
-    Route::put('/task/{id}', [TaskController::class, 'update']); // Modifier une tâche (admin)
-    Route::delete('/task/{id}', [TaskController::class, 'destroy']); // Supprimer une tâche (admin)
+    Route::put('/tasks/{id}', [TaskController::class, 'update']); // Modifier une tâche (admin)
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']); // Supprimer une tâche (admin)
 });
 
 Route::middleware('auth:sanctum')->post('/logout', [AuthenticatedSessionController::class, 'destroy']);
